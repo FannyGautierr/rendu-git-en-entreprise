19f83aab95b7c37c02a9ebf59ef686490d3b91a5
lucaschrng <113120989+lucaschrng@users.noreply.github.com>
2024-11-05 10:03:19 +0100

    feat: calculator (#9)
    * feat: setup calulator UI
    
    * feat: add calculator logic

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
+import { setupCalculator } from './calculator';
 import './style.css';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    Homepage
+    <input
+      type="number"
+      id="x"
+      class="bg-neutral-700 border border-neutral-500 rounded p-2 w-24"
+    />
+    <select
+      id="operator"
+      class="bg-neutral-700 border border-neutral-500 rounded p-2"
+    >
+      <option value="+">&#43;</option>
+      <option value="-">&#8722;</option>
+      <option value="*">&#215;</option>
+      <option value="/">&#247;</option>
+    </select>
+    <input
+      type="number"
+      id="y"
+      class="bg-neutral-700 border border-neutral-500 rounded p-2 w-24"
+    />
+    <span>&#61;</span>
+    <span id="result" class="bg-neutral-700 border border-neutral-500 rounded p-2"></span>
   </div>
 `;
+
+setupCalculator();
